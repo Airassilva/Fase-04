0c32dd6527e4b5c2bbf742d9cc9139d250807862
Aira <airasoares17@gmail.com>
2026-02-17 18:28:12 -0300

    Adjust to day aggregation and tests for ProcessorService

--- feedback-system/processor/src/main/java/dev/aira/lambda/processor/service/ProcessorService.java
+++ feedback-system/processor/src/main/java/dev/aira/lambda/processor/service/ProcessorService.java
@@ -32,16 +32,21 @@ public class ProcessorService {
             FeedbackMessage  feedbackMessage = objectMapper.readValue(message,FeedbackMessage.class);
             log.info("[PROCESSAR FEEDBACK] {}", feedbackMessage);
 
+            LocalDate dia = Instant.parse(feedbackMessage.getDataEnvio())
+                    .atZone(ZoneOffset.UTC)
+                    .toLocalDate();
+            String chave = dia.toString();
+
             Relatorio relatorio = new Relatorio(
                     "FEEDBACK",
                     feedbackMessage.getFeedbackId(),
                     feedbackMessage.getUrgencia(),
                     feedbackMessage.getDescricao(),
-                    feedbackMessage.getDataEnvio()
+                    chave
             );
 
             persistirDados(relatorio);
-            atualizarAgregacaoPorDia(feedbackMessage.getDataEnvio());
+            atualizarAgregacaoPorDia(chave);
             atualizarAgregacaoPorUrgencia(feedbackMessage.getUrgencia());
 
         }catch (JsonProcessingException e) {
@@ -54,17 +59,12 @@ public class ProcessorService {
     }
 
     private void atualizarAgregacaoPorDia(String dataEnvio) {
-        LocalDate dia = Instant.parse(dataEnvio)
-                .atZone(ZoneOffset.UTC)
-                .toLocalDate();
-        String chave = dia.toString();
-
-        Relatorio agregacao = relatorioRepository.obterPoTipoEChave("AGR_DIA", chave);
+        Relatorio agregacao = relatorioRepository.obterPoTipoEChave("AGR_DIA", dataEnvio);
 
         if (agregacao == null) {
             agregacao = new Relatorio();
             agregacao.setTipo("AGR_DIA");
-            agregacao.setChave(chave);
+            agregacao.setChave(dataEnvio);
             agregacao.setQuantidade(1);
         } else {
             agregacao.setQuantidade(agregacao.getQuantidade() + 1);
